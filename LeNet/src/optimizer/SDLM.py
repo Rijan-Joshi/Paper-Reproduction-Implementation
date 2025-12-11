f7b9d48ae5170b81c10eca668993217ed60c2a9f
Rijan-Joshi <csaijspy@gmail.com>
2025-12-11 09:06:28 +0545

    SDLM

---
+++ LeNet/src/optimizer/SDLM.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class SDLM:
+    """
+        Stochastic Diagonal Levenbeg Marquardt Method Implementation for Optimization of the Network
+    """
+
+    def __init__(self, lr = 1.0, mu = 0.01, decay = 0.05, eps = 1e-8):
+        self.lr = lr
+        self.damping = mu
+        self.weight_decay = decay
+        self.eps = eps
+        self.hessian_diag = {}
+        self.lr_multipliers = {}
+    
+    
